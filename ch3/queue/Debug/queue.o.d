cb1563b0905fa90c799ccb6777bfd6bd7559f633
mcxiaoke <mcxiaoke@gmail.com>
2012-07-05 16:46:12 +0800

    clean ch3 workspace

--- ch3/queue/Debug/queue.o.d
+++
@@ -1,4 +0,0 @@
-Debug/queue.o: E:/msys/home/mcxiaoke/dsaac/ch3/queue/queue.c \
- E:/msys/home/mcxiaoke/dsaac/ch3/queue/queue.h
-
-E:/msys/home/mcxiaoke/dsaac/ch3/queue/queue.h:
--- ch3/queue_use_list/Debug/queue2.o.d
+++
@@ -1,4 +0,0 @@
-Debug/queue2.o: E:/msys/home/mcxiaoke/dsaac/ch3/queue_use_list/queue2.c \
- E:/msys/home/mcxiaoke/dsaac/ch3/queue_use_list/queue2.h
-
-E:/msys/home/mcxiaoke/dsaac/ch3/queue_use_list/queue2.h:
