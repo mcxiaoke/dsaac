23ce33fd29be627879ea1aa1afc2073065f4cc1c
mcxiaoke <mcxiaoke@gmail.com>
2012-08-02 17:08:28 +0800

    add avl project.

---
+++ ch4/avl/avl.h
@@ -0,0 +1,17 @@
+/*
+ * avl.h
+ *
+ *  Created on: 2012-8-2
+ *      Author: mcxiaoke
+ */
+
+#ifndef AVL_H_
+#define AVL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+#endif /* AVL_H_ */
